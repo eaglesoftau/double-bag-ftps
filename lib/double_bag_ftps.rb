bb09328bde8464fd8cfdd6b090f7fc8aeccb51e2
= <phil@philmunt.com>
2019-03-29 06:39:28 +1100

    fix: ruby 2.4

--- lib/double_bag_ftps.rb
+++ lib/double_bag_ftps.rb
@@ -15,16 +15,17 @@ class DoubleBagFTPS < Net::FTP
   attr_reader :ftps_mode
 
   # The OpenSSL::SSL::SSLContext to use for creating all OpenSSL::SSL::SSLSocket objects.
-  attr_accessor :ssl_context
+  attr_accessor :ssl_context_doublebag
 
   def initialize(host = nil, user = nil, passwd = nil, acct = nil, ftps_mode = EXPLICIT, ssl_context_params = {})
     raise ArgumentError unless valid_ftps_mode?(ftps_mode)
+
     @ftps_mode = ftps_mode
-    @ssl_context = DoubleBagFTPS.create_ssl_context(ssl_context_params)
+    @ssl_context_doublebag = DoubleBagFTPS.create_ssl_context(ssl_context_params)
     super(host, user, passwd, acct)
   end
 
-  def DoubleBagFTPS.open(host, user = nil, passwd = nil, acct = nil, ftps_mode = EXPLICIT, ssl_context_params = {})
+  def self.open(host, user = nil, passwd = nil, acct = nil, ftps_mode = EXPLICIT, ssl_context_params = {})
     if block_given?
       ftps = new(host, user, passwd, acct, ftps_mode, ssl_context_params)
       begin
@@ -42,8 +43,9 @@ class DoubleBagFTPS < Net::FTP
   #
   def ftps_mode=(ftps_mode)
     # Ruby 1.8.7/1.9.2 compatible check
-    if (defined?(NullSocket) && @sock.kind_of?(NullSocket)) || @sock.nil? || @sock.closed?
+    if (defined?(NullSocket) && @sock.is_a?(NullSocket)) || @sock.nil? || @sock.closed?
       raise ArgumentError unless valid_ftps_mode?(ftps_mode)
+
       @ftps_mode = ftps_mode
     else
       raise 'Cannot set ftps_mode while connected'
@@ -56,6 +58,7 @@ class DoubleBagFTPS < Net::FTP
   #
   def connect(host, port = ftps_implicit? ? IMPLICIT_PORT : FTP_PORT)
     @hostname = host
+    @sock = BufferedSocket.new(@bare_sock, read_timeout: @read_timeout)
     super
   end
 
@@ -66,9 +69,9 @@ class DoubleBagFTPS < Net::FTP
         @sock = ssl_socket(@sock)
       end
     end
-    
+
     super(user, passwd, acct)
-    voidcmd('PBSZ 0') # The expected value for Protection Buffer Size (PBSZ) is 0 for TLS/SSL 
+    voidcmd('PBSZ 0') # The expected value for Protection Buffer Size (PBSZ) is 0 for TLS/SSL
     voidcmd('PROT P') # Set data channel protection level to Private
   end
 
@@ -77,13 +80,13 @@ class DoubleBagFTPS < Net::FTP
   # when using implicit FTPS
   #
   def open_socket(host, port, defer_implicit_ssl = false)
-    if defined? SOCKSSocket and ENV["SOCKS_SERVER"]
+    if defined? SOCKSSocket && ENV['SOCKS_SERVER']
       @passive = true
       sock = SOCKSSocket.open(host, port)
     else
       sock = TCPSocket.open(host, port)
     end
-    return (!defer_implicit_ssl && ftps_implicit?) ? ssl_socket(sock) : sock
+    !defer_implicit_ssl && ftps_implicit? ? ssl_socket(sock) : sock
   end
   private :open_socket
 
@@ -94,40 +97,34 @@ class DoubleBagFTPS < Net::FTP
     if @passive
       host, port = makepasv
 
-      if @resume and rest_offset
+      if @resume && rest_offset
         resp = sendcmd('REST ' + rest_offset.to_s)
-        if resp[0] != ?3
-          raise FTPReplyError, resp
-        end
+        raise FTPReplyError, resp if resp[0] != '3'
       end
       conn = open_socket(host, port, true)
       resp = sendcmd(cmd)
       # skip 2XX for some ftp servers
-      resp = getresp if resp[0] == ?2
-      if resp[0] != ?1
-        raise FTPReplyError, resp
-      end
+      resp = getresp if resp[0] == '2'
+      raise FTPReplyError, resp if resp[0] != '1'
+
       conn = ssl_socket(conn) # SSL connection now possible after cmd sent
     else
       sock = makeport
       sendport(sock.addr[3], sock.addr[1]) if sendport_needed?
-      if @resume and rest_offset
+      if @resume && rest_offset
         resp = sendcmd('REST ' + rest_offset.to_s)
-        if resp[0] != ?3
-          raise FTPReplyError, resp
-        end
+        raise FTPReplyError, resp if resp[0] != '3'
       end
       resp = sendcmd(cmd)
       # skip 2XX for some ftp servers
-      resp = getresp if resp[0] == ?2
-      if resp[0] != ?1
-        raise FTPReplyError, resp
-      end
+      resp = getresp if resp[0] == '2'
+      raise FTPReplyError, resp if resp[0] != '1'
+
       conn = sock.accept
       conn = ssl_socket(conn)
       sock.close
     end
-    return conn
+    conn
   end
   private :transfercmd
 
@@ -137,15 +134,20 @@ class DoubleBagFTPS < Net::FTP
   # to Ruby 2.1 since version 2.1.7.
   def sendport_needed?
     @sendport_needed ||= begin
-      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.2.3") ||
-      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.1.7") &&
-      Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.2.0")
+      Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.2.3') ||
+        Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.1.7') &&
+          Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.2.0')
     end
   end
   private :sendport_needed?
 
-  def ftps_explicit?; @ftps_mode == EXPLICIT end
-  def ftps_implicit?; @ftps_mode == IMPLICIT end
+  def ftps_explicit?
+    @ftps_mode == EXPLICIT
+  end
+
+  def ftps_implicit?
+    @ftps_mode == IMPLICIT
+  end
 
   def valid_ftps_mode?(mode)
     mode == EXPLICIT || mode == IMPLICIT
@@ -157,19 +159,18 @@ class DoubleBagFTPS < Net::FTP
   #
   def ssl_socket(sock)
     raise 'SSL extension not installed' unless defined?(OpenSSL)
-    sock = OpenSSL::SSL::SSLSocket.new(sock, @ssl_context)
-    if @ssl_session
-      sock.session = @ssl_session
-    end
+
+    sock = OpenSSL::SSL::SSLSocket.new(sock, @ssl_context_doublebag)
+    sock.session = @ssl_session if @ssl_session
     sock.sync_close = true
     sock.connect
     print "get: #{sock.peer_cert.to_text}" if @debug_mode
-    unless @ssl_context.verify_mode == OpenSSL::SSL::VERIFY_NONE
+    unless @ssl_context_doublebag.verify_mode == OpenSSL::SSL::VERIFY_NONE
       sock.post_connection_check(@hostname)
     end
     @ssl_session = sock.session
     decorate_socket sock
-    return sock
+    sock
   end
   private :ssl_socket
 
@@ -181,12 +182,14 @@ class DoubleBagFTPS < Net::FTP
   # closed.
 
   def decorate_socket(sock)
-
-    def sock.shutdown(how)
+    def sock.shutdown(_how)
       @shutdown = true
     end
 
-    def sock.read_timeout=(seconds)
+    def sock.read_timeout=(seconds); end
+
+    def sock.remote_address
+      io.remote_address
     end
 
     # Skip read after shutdown.  Prevents 2.0 from hanging in
@@ -194,17 +197,17 @@ class DoubleBagFTPS < Net::FTP
 
     def sock.read(*args)
       return if @shutdown
+
       super(*args)
     end
-
   end
   private :decorate_socket
 
-  def DoubleBagFTPS.create_ssl_context(params = {})
+  def self.create_ssl_context(params = {})
     raise 'SSL extension not installed' unless defined?(OpenSSL)
+
     context = OpenSSL::SSL::SSLContext.new
     context.set_params(params)
-    return context
+    context
   end
-
 end
